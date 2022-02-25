cdf81ed5ca98c8f6be2a876d0dcb3eb4e05fb398
ayaangazali <ayaangazali.work@gmail.com>
2022-02-25 02:39:58 -0800

    single completion call

--- chatbot.py
+++ chatbot.py
@@ -5,3 +5,13 @@ import openai
 
 load_dotenv()
 openai.api_key = os.getenv("OPENAI_API_KEY")
+
+
+def get_answer(prompt):
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=prompt,
+        max_tokens=512,
+        temperature=0.7,
+    )
+    return response.choices[0].text.strip()
