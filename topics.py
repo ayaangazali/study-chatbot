62ba834a1b34df64cb0835c686e993763efb92fa
ayaangazali <ayaangazali.work@gmail.com>
2022-07-13 13:44:18 -0700

    quiz builder

--- topics.py
+++ topics.py
@@ -6,3 +6,11 @@ def explain_concept(topic):
         "Explain the concept of %s to a student who is new to it. "
         "Use simple language and give a short everyday example." % topic
     )
+
+
+def make_quiz(topic, num_questions=5):
+    return (
+        "Create a %d question quiz about %s. "
+        "Number each question, and put the answers in an answer key at the bottom."
+        % (num_questions, topic)
+    )
